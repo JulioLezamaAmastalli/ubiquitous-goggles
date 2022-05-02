444d9b27d97d903cb66400a4960eb907a1c49285
mig-calval <mig.calval@gmail.com>
2022-05-01 23:02:12 -0500

    Added modifications to predict.py to update the h2h.prediction table

--- Dags/dags-predict/predict.py
+++ Dags/dags-predict/predict.py
@@ -39,4 +39,42 @@ from model_prediction import CustomModelPrediction
 classifier = CustomModelPrediction.from_path(path)
 results = classifier.predict(X)
 
-print(results)
+# Print the results 
+for localteam, visitorteam, result in zip(X['localteam_id'].values, X['visitorteam_id'].values, results):
+    print(f'Probability of localteam {localteam} winning vs visitorteam {visitorteam}: ' + str(round(100*result[0], 2)) + '%')
+
+### We will add the predictions bask to the DB. For this, we
+### convert the results to a string
+to_predict = X.copy()
+to_predict['probs'] = results
+to_predict['probs'] = to_predict['probs'].apply(lambda x: str(x[0]) + ',' + str(x[1]))
+
+def list_of_tuples(df):
+    
+    all_values = []
+    
+    for k in range(df.shape[0]):
+        temp = df.copy()
+        temp = temp.reset_index()
+        temp = temp[['probs', 'id']]
+        temp = temp.iloc[k]                        
+        temp = temp.astype(str)
+        temp = tuple(temp)
+        all_values.append(temp)
+        
+    return all_values
+
+to_predict_values = list_of_tuples(to_predict)
+
+### Since the table already exists, we update the na values
+### in it with the probabilities we calculated
+sql_com = "UPDATE h2h.prediction SET probs = %s WHERE id = %s"
+
+for value in to_predict_values:
+    try:
+        cursor.execute(sql_com, value)
+    except mysql.connector.IntegrityError as err:
+        print("Something went wrong: {}".format(err))        
+        pass
+
+client.commit()
