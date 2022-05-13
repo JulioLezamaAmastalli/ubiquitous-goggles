6c854fc29e55d81e691cda01f8dc2a4b0abf1237
mig-calval <mig.calval@gmail.com>
2022-05-13 16:31:51 -0500

    Added endpoint prediction function to script

--- pipeline/variables_n_functions.py
+++ pipeline/variables_n_functions.py
@@ -1,4 +1,5 @@
 import requests
+import googleapiclient.discovery
 
 ############################## Variables ##############################
 
@@ -37,8 +38,6 @@ columnas_df = [
 
 ############################## Functions ##############################
 
-### IMPORTANTE!!! HAY QUE AniADIR LA VENTANA DE TIEMPO
-### COMO PARAMETRO
 def head2head(id1, id2, sports_key, between = None):
     """
     Return the historical match results and characteristics between any 2 given teams
@@ -79,3 +78,42 @@ def booleanize(x):
         return 0
     else:
         return -1
+
+### Function to make predictions to the AI-Platform endpoint
+
+# https://cloud.google.com/ai-platform/prediction/docs/online-predict#python
+
+# Create the AI Platform service object.
+# To authenticate set the environment variable
+# GOOGLE_APPLICATION_CREDENTIALS=<path_to_service_account_file>
+service = googleapiclient.discovery.build('ml', 'v1')
+
+def predict_json(project, model, instances, version=None):
+    """Send json data to a deployed model for prediction.
+
+    Args:
+        project (str): project where the AI Platform Model is deployed.
+        model (str): model name.
+        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
+            your deployed model expects as inputs. Values should be datatypes
+            convertible to Tensors, or (potentially nested) lists of datatypes
+            convertible to tensors.
+        version: str, version of the model to target.
+    Returns:
+        Mapping[str: any]: dictionary of prediction results defined by the
+            model.
+    """
+    name = 'projects/{}/models/{}'.format(project, model)
+
+    if version is not None:
+        name += '/versions/{}'.format(version)
+
+    response = service.projects().predict(
+        name=name,
+        body={'instances': instances}
+    ).execute()
+
+    if 'error' in response:
+        raise RuntimeError(response['error'])
+
+    return response['predictions']
