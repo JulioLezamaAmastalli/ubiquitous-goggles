9b1116ac9a70bae48d00d47275fa93349b00d246
Marco Ramos <57338890+marcoyel21@users.noreply.github.com>
2022-05-16 01:44:49 -0500

    modificacion para nuevo modelo

--- final_pipelines/deployment/html_factory.py
+++ final_pipelines/deployment/html_factory.py
@@ -27,15 +27,15 @@ cursor = client.cursor()
 from datetime import date, timedelta
 import numpy as np
 
-
-end = date.today()
-start = end - timedelta(7)
+start = date.today()
+end = start + timedelta(7)
 
 end = end.strftime("%Y-%m-%d")
 start = start.strftime("%Y-%m-%d")
 
 between = '"'+start+'"' + ' and ' + '"'+end+'"'
-data_request_string='SELECT * FROM h2h.model2 WHERE (match_day BETWEEN ' +between+ ')'
+
+data_request_string='SELECT * FROM h2h.model2_predictions WHERE (match_day BETWEEN ' +between+ ')'
 df=pd.read_sql(data_request_string, con=client)
 
 ###############################################
@@ -49,23 +49,30 @@ catalogue=pd.read_csv("teams_flags.csv")
 catalogue['team']=catalogue['team'].astype('int64')
 df['localteam_id']=df['localteam_id'].astype('int64')
 df['visitorteam_id']=df['visitorteam_id'].astype('int64')
-
+df[['localteam_win_p', 'visitor_tie_or_win_p']]=df['probs'].str.split(',', expand=True)
+df['localteam_win_p']=df['localteam_win_p'].astype('float64')
+df['visitor_tie_or_win_p']=df['visitor_tie_or_win_p'].astype('float64')
+df=df.round(3)
 ####
 #MERGE
 df=pd.merge(df, catalogue, left_on='localteam_id', right_on='team')
 df=pd.merge(df, catalogue, left_on='visitorteam_id', right_on='team')
-df
+
+
+
 # COLNAMES
-df.columns = ['id', 'Y', 'league_id', 'season_id', 'venue_id', 'referee_id',
+df.columns = ['id', 'probs', 'league_id', 'season_id', 'venue_id', 'referee_id',
        'localteam_id', 'visitorteam_id', 'localteam_position',
-      'visitorteam_position', 'match_day',"x","x2","localteam_flag",
+      'visitorteam_position', 'match_day',"localteam_win_p","visitor_tie_or_win_p","x","x2","localteam_flag",
         "localteam_name","x3","x4","visitorteam_flag","visitorteam_name"]
 
 ######## CREO EL DATAFRAME QUE VOY A MOSTRAR ######
-output= df[['Y','localteam_flag','localteam_name','visitorteam_flag','visitorteam_name','match_day']]
+output= df[['localteam_win_p','localteam_flag','localteam_name',"visitor_tie_or_win_p",'visitorteam_flag','visitorteam_name','match_day']]
 
 ####### CREO EL HTML ######
 from IPython.core.display import HTML
 def path_to_image_html(path):
     return '<img src="'+ path + '" width="60" >'
-output.to_html('index.html',escape=False, formatters=dict(localteam_flag=path_to_image_html, visitorteam_flag=path_to_image_html))
+output.to_html('index.html',escape=False, 
+               formatters=dict(localteam_flag=path_to_image_html,visitorteam_flag=path_to_image_html))
+
